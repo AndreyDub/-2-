824a9e6ac727e266be1ce16476ee363b20e94c5e
AndreyDub <145935249+AndreyDub@users.noreply.github.com>
2023-09-24 23:25:38 +0300

    Add files via upload

---
+++ УСР-2-Андрей Д20/ConsoleApp20/Program.cs
@@ -0,0 +1,60 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N1, N2, result;
+            char option;
+
+            Console.Write("Введите первое число: ");
+            N1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            N2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Калькулятор");
+            Console.WriteLine("1. Сложение");
+            Console.WriteLine("2. Вычитание");
+            Console.WriteLine("3. Умножение");
+            Console.WriteLine("4. Деление");
+
+            Console.Write("Введите номер операции, которую вы хотите выполнить: ");
+            option = Convert.ToChar(Console.ReadLine());
+
+            switch (option)
+            {
+                case '1':
+                    result = N1 + N2;
+                    Console.WriteLine($"Результат сложения: {result}");
+                    break;
+
+                case '2':
+                    result = N1 - N2;
+                    Console.WriteLine($"Результат вычитания: {result}");
+                    break;
+
+                case '3':
+                    result = N1 * N2;
+                    Console.WriteLine($"Результат умножения: {result}");
+                    break;
+
+                case '4':
+                    result = N1 / N2;
+                    Console.WriteLine($"Результат деления: {result}");
+                    break;
+
+                default:
+                    Console.WriteLine("Неверный ввод");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
